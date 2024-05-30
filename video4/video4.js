41d270814e72d5dac387da593b1c901872bf0c1c
CrisBean <4bhandmade@gmail.com>
2024-05-30 10:22:25 +0700

    source code video10

---
+++ video4/video4.js
@@ -0,0 +1,4 @@
+let a=`i'm the string`
+let text = `we are the so-called 'vilkings' from the north`
+console.log(text)
+console.log(a)
---
+++ video9/video9.js
@@ -0,0 +1,13 @@
+console.log('Hello team PO clb'.toUpperCase());
+let b= {name: 'Bean' , age: 28 , gender: 'female' };
+let a = ['Bean','huong','nghia','tuan'];
+console.log('captain team is '.toUpperCase(),a[0])
+console.log(b);
+console.log(a)
+let syll=[
+    {name :'Bean',age:28, gender:'female'},
+    {name : 'Phuong',age:34,gender:'female'},
+    {name:'Nghia',age:28,gender:'female'}
+]
+console.log(`danh sach thanh vien PO PHAT DAT : `,syll ,
+ `\nTrong do chu tich cua doi la:`,syll[1])
